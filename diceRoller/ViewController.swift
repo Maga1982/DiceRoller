de3540f22967ef439e26b15e6f4873443687dff4
Lax Mam <laxmam@Laxs-MacBook-Air.local>
2019-02-01 15:13:47 -0500

    Signed-off-by: Lax Mam <laxmam@Laxs-MacBook-Air.local>
    my first ios App DiceRoller:x!

--- diceRoller/ViewController.swift
+++ diceRoller/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var index1 : Int = 0
+    var index2 : Int = 0
+    
+    @IBOutlet weak var imageOne: UIImageView!
+    
+    @IBOutlet weak var imageTwo: UIImageView!
+    var diceArray = ["Dice1","Dice2","Dice3","Dice4","Dice5","Dice6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        rollerDice()
     }
-
-
+    
+    @IBAction func rollerDice() {
+        print("roll button")
+        index1 = Int(arc4random_uniform(6))
+        index2 = Int(arc4random_uniform(6))
+        
+        imageOne.image=UIImage(named: diceArray[index1])
+        imageTwo.image=UIImage(named: diceArray[index2])
+    }
+    
+    
+    
 }
 
